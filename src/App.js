519195be4c79c0aaaee2f7d676570609223462c4
minhyepark <minhyepark.dev@gmail.com>
2022-01-14 21:41:07 +0900

    PropsDatatype 자료형

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
-import Props from "./R017_Props";
+import PropsDatatype from "./R018_PropsDatatype";
+
 function App() {
     return (
         <div className="App">
-            <Props props_val="THIS IS PROPS" />
+            <PropsDatatype
+                String="react"
+                Number={200}
+                Boolean={1 == 1}
+                Array={[0, 1, 8]}
+                ObjectJson={{ react: "리액트", twohundred: "200" }}
+                Function={console.log("FunctionProps: function!")}
+            />
         </div>
     );
 }
