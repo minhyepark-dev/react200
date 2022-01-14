ab84696c71472204924f4c9fb7a6f10df9b8fd77
minhyepark <minhyepark.dev@gmail.com>
2022-01-14 21:54:32 +0900

    propTypes isRequired

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import PropsObjVal from "./R020_PropsObjVal";
+import PropsRequired from "./R021_PropsRequired";
 
 function App() {
     return (
         <div className="App">
-            <PropsObjVal ObjectJson={{ react: "리액트", twohundred: "200" }} />
+            <PropsRequired ReactString={"Hello"} ReactNumber={200} />
         </div>
     );
 }
