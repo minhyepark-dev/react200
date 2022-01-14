c1598049ebffe72afdd425647b9e31c3d3957f02
minhyepark <minhyepark.dev@gmail.com>
2022-01-14 21:43:38 +0900

    PropsBoolean

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import PropsDatatype from "./R018_PropsDatatype";
+import PropsBoolean from "./R019_PropsBoolean";
 
 function App() {
     return (
         <div className="App">
-            <PropsDatatype
-                String="react"
-                Number={200}
-                Boolean={1 == 1}
-                Array={[0, 1, 8]}
-                ObjectJson={{ react: "리액트", twohundred: "200" }}
-                Function={console.log("FunctionProps: function!")}
-            />
+            <PropsBoolean BooleanTrueFalse={false} />
+            <PropsBoolean BooleanTrueFalse />
         </div>
     );
 }
