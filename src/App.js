af0429bcc128f2bae7eceb081666494918a2eaf3
minhyepark <minhyepark.dev@gmail.com>
2022-01-15 14:37:02 +0900

    onChange

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactonClick from "./R066_onClick";
+import ReactChange from "./R067_onChange";
 
 function App() {
     return (
         <div>
             <h1>Start React 200!</h1>
-            <ReactonClick />
+            <ReactChange />
         </div>
     );
 }
