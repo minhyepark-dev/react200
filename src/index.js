a7a4bd0e29c1bffa6d3489dac42c21999ae148e1
minhyepark <minhyepark.dev@gmail.com>
2022-01-16 00:11:11 +0900

    react-redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 import reducers from "./reducers";
 
 const store = createStore(reducers);
 
 const listener = () => {
-    ReactDOM.render(<App store={store} />, document.getElementById("root"));
+    ReactDOM.render(
+        <Provider store={store}>
+            <App indexProp="react" />
+        </Provider>,
+        document.getElementById("root")
+    );
 };
 
 store.subscribe(listener);
